e8a7a40130c892da5c04af0a6bd5bbfcda4b4988
Ximo79 <111804597+Ximo79@users.noreply.github.com>
2025-08-08 19:44:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
-  build: {
-    // Phaser a un chunk separado
-    rollupOptions: {
-      output: { manualChunks: { phaser: ['phaser'] } }
-    },
-    chunkSizeWarningLimit: 2000
-  }
+  base: './'
 });
