85f27ed0b173cf3e66c2d1d5b7125aa206b2a192
Ximo79 <111804597+Ximo79@users.noreply.github.com>
2025-08-08 16:25:01 -0400

    Update GameScene.ts

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -1,20 +1,4 @@
-import Phaser from 'phaser';
-
-export class GameScene extends Phaser.Scene {
-  private player!: Phaser.Physics.Arcade.Sprite;
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-
-  constructor() {
-    super('GameScene');
-  }
-
-  preload() {
-    // Carga de imágenes desde la carpeta pública (public/assets)
-    this.load.image('mapa', 'assets/map_1_court.png');
-    this.load.image('breijo', 'assets/sprite_1_front.png');
-  }
-
- create() {
+create() {
   // Cargar fondo y escalarlo
   const fondo = this.add.image(0, 0, 'mapa').setOrigin(0, 0).setScale(3); // Ajusta si es necesario
   const width = fondo.width * fondo.scaleX;
@@ -33,23 +17,4 @@ export class GameScene extends Phaser.Scene {
 
   // Controles
   this.cursors = this.input.keyboard.createCursorKeys();
-}
-
-
-  update() {
-    const speed = 200;
-    this.player.setVelocity(0);
-
-    if (this.cursors.left?.isDown) {
-      this.player.setVelocityX(-speed);
-    } else if (this.cursors.right?.isDown) {
-      this.player.setVelocityX(speed);
-    }
-
-    if (this.cursors.up?.isDown) {
-      this.player.setVelocityY(-speed);
-    } else if (this.cursors.down?.isDown) {
-      this.player.setVelocityY(speed);
-    }
-  }
 }
