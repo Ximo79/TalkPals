72af9bc01dd07a3432c4cc2cd4c7a828919ed34b
Ximo79 <111804597+Ximo79@users.noreply.github.com>
2025-08-08 14:44:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'src',
-  base: './',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  }
+  base: './'
 })
