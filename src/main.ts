ad478c8046077cedd2b1260577620dabc386ed72
Ximo79 <111804597+Ximo79@users.noreply.github.com>
2025-08-08 18:26:48 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,7 @@ import { GameScene } from './GameScene';
 new Phaser.Game({
   type: Phaser.AUTO,
   pixelArt: true,
-  physics: { default: 'arcade', arcade: { gravity: { y: 0 } } },
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 1280,   // base lógica; se adapta al viewport
-    height: 720
-  },
+  physics: { default: 'arcade', arcade: { gravity: { y: 900 } } }, // ← gravedad para salto
+  scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH, width: 1280, height: 720 },
   scene: [GameScene]
 });
