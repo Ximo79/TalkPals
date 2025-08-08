e7e225d2f728fc7d76f74f3a677271459bb364d6
Ximo79 <111804597+Ximo79@users.noreply.github.com>
2025-08-08 18:03:22 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
-import Phaser from 'phaser'
-import { GameScene } from './GameScene'
+import Phaser from 'phaser';
+import { GameScene } from './GameScene';
 
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 }
-    }
-  },
-  scene: [GameScene]
-})
+  pixelArt: true,
+  physics: { default: 'arcade', arcade: { gravity: { y: 0 } } },
+  scene: [GameScene],
+});
