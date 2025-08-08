6536b6487e33385460c6b756bc722605c5e05f3e
Ximo79 <111804597+Ximo79@users.noreply.github.com>
2025-08-08 18:10:19 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import Phaser from 'phaser';
-import { GameScene } from './GameScene';
 
-new Phaser.Game({
+// Crear juego sin escenas; las cargamos por import() para partir el bundle
+const game = new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   pixelArt: true,
   physics: { default: 'arcade', arcade: { gravity: { y: 0 } } },
-  scene: [GameScene],
+  scene: []
+});
+
+// Code-splitting: carga diferida de la escena principal
+import('./GameScene').then(({ GameScene }) => {
+  // @ts-ignore
+  game.scene.add('GameScene', GameScene, true);
 });
